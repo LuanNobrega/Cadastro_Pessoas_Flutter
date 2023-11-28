84402c11939020e83a548c2068f3154a8565ed56
LuanNobrega <luan.snobrega@gmail.com>
2023-11-28 18:27:08 -0300

    Retirando a marca debug

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,15 @@ import 'cadastro/home_page.dart';
 import 'package:provider/provider.dart';
 import 'cadastro/record_provider.dart';
 
+//Define a estrutura básica do aplicativo Flutter
+//Definindo as rotas para as diferentes páginas da aplicação.
+
 void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => RecordProvider(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         initialRoute: '/home',
         routes: {
           '/home': (context) => HomePage(),
